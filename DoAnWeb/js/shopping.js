1597bd235fb8696f60710fe6775286bdc4db233a
MinhDuc0712 <151363933+MinhDuc0712@users.noreply.github.com>
2024-11-04 16:22:15 +0700

    Add files via upload

---
+++ DoAnWeb/js/shopping.js
@@ -0,0 +1,8 @@
+// JavaScript
+function addToCart() {
+    // Thực hiện các thao tác cần thiết trước khi chuyển hướng, như thêm sản phẩm vào giỏ hàng
+    
+    // Chuyển hướng sang trang giỏ hàng
+    window.location.href = "cart.html";
+  }
+  
