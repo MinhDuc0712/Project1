0f8c09ebf02781ac43eed62a9c2b0f38ff1757dc
MinhDuc0712 <151363933+MinhDuc0712@users.noreply.github.com>
2024-11-04 16:32:25 +0700

    Delete DoAnWeb directory

--- DoAnWeb/js/shopping.js
+++
@@ -1,8 +0,0 @@
-// JavaScript
-function addToCart() {
-    // Thực hiện các thao tác cần thiết trước khi chuyển hướng, như thêm sản phẩm vào giỏ hàng
-    
-    // Chuyển hướng sang trang giỏ hàng
-    window.location.href = "cart.html";
-  }
-  
